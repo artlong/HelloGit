52c13093fc630abd61d33a4591c4e36282bbfb6d
artlong <artlonglong@gmail.com>
2014-08-05 11:59:00 +0800

    test commit

---
+++ src/tw/com/artlong/HelloWorld.java
@@ -0,0 +1,13 @@
+package tw.com.artlong;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git Ya!");
+		System.out.println("Hello Git Ya JP!");
+	}
+
+}
