caf1cc6f441cff653c686ce7ac7baa78d08f2a1e
artlong <artlonglong@gmail.com>
2014-08-05 14:28:51 +0800

    commit java

--- src/tw/com/artlong/HelloWorld.java
+++ src/tw/com/artlong/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Ya!");
 		System.out.println("Hello Git Ya JP!");
+		System.out.println("Hello Git Ya JPaaaa!");
 	}
 
 }
